4387b4d73d544de91e21ff9157a18685097e05e5
Jarviss77 <gupta.shubham2404@gmail.com>
2024-10-16 20:50:46 +0530

    ffffs

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 const Sidebar = () => {
   return (
     <div className="bg-zinc-900 text-white h-screen p-5 pt-5">
-      <div className="text-2xl font-semibold mb-4">NEXUS</div>
+      <div className="text-2xl font-semibold mb-4">Game Theory</div>
       <div className="pt-3">
         <div className="mb-2 p-2 hover:bg-slate-800 rounded-md px-4">
           <Link to="/" className=" flex items-center">
